d3477995c78fd163cd002f08e5ad361cc7a8b286
Bohdan <bogukraine@gmail.com>
2024-12-04 22:00:44 +0200

    db connection foundation

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type Response struct {
@@ -17,11 +22,31 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	connectionString := os.Getenv("DB_CONNECTION_STRING")
+	if connectionString == "" {
+		log.Fatal("DB_CONNECTION_STRING environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
 	http.HandleFunc("/hello", helloHandler)
 
 	log.Println("Starting server on :8080")
 
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
